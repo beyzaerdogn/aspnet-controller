ee2af7434d3de0efe704817150a0b6c45dd39224
BEYZA ERDOĞAN <BEYZA ERDOAN@BEYZAERDOGAN>
2023-09-23 12:46:40 +0300

    Proje dosyası ekle.

---
+++ WebApplication1/Controllers/OrnekController1.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class OrnekController : Controller
+    {
+        public IActionResult Index2()
+        {
+           return View();
+        }
+        public IActionResult ContentResult()
+        {
+            return Content("ContentResult");
+        }
+    }
+}
